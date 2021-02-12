378737ef08553471b30619f830e42ecbd6886c50
Amir <67755404+Amir228322@users.noreply.github.com>
2021-02-11 19:43:46 -0800

    Adding activity with my profile and changing the title

--- app/src/main/java/com/example/profiles_assignemnt/Amir_Mokhammad.java
+++ app/src/main/java/com/example/profiles_assignemnt/Amir_Mokhammad.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Amir_Mokhammad extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("Group I");
+        setContentView(R.layout.activity_amir__mokhammad);
     }
 }
